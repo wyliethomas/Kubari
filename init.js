3efc078ad1faec22d13e48bf5b315ed6daa2cb33
Doug Weber <dweber@reisenderlabs.net>
2010-12-12 16:10:00 -0700

    Updated when init defines the init function (when file loads instead of doc ready).

--- init.js
+++ init.js
@@ -1,26 +1,21 @@
 (function($) {
-  $(function() {
+  $.extend(Fr.plugin.methods,{
+    init: function(app_name,app_root) {
+      Fr[app_name] = this; // keep the context of the framework
+      this.data('AppRoot',app_root);
 
-    $.extend(Fr.plugin.methods,{
-      init: function(app_name,app_root) {
-        Fr[app_name] = this; // keep the context of the framework
-        this.data('AppRoot',app_root);
+      // load the views into the framework
+      this.data('_views',{});
+      this.framework("_loadViews_");
 
-        // load the views into the framework
-        this.data('_views',{});
-        this.framework("_loadViews_");
+      // initialize the controllers
+      this.framework('_loadControllers_',app_name);
 
-        // initialize the controllers
-        this.framework('_loadControllers_',app_name);
-
-        // render the entry point view
-        this.framework('views',"layouts/application",function(view) {
-          Fr[app_name].framework('renderAsLayout',view);
-        });
-      }
-    });
-
-    //$(document).framework(); // call init
-
+      // render the entry point view
+      this.framework('views',"layouts/application",function(view) {
+        Fr[app_name].framework('renderAsLayout',view);
+      });
+    }
   });
+  //$(function() { $(document).framework('init','app_name','app_root'); }); // example init call
 })(jQuery);
