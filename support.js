813c0a530dadb132295a7eb0f6dcc2926fd7230a
Doug Weber <doug.weber@youpublish.com>
2011-01-31 13:20:29 -0700

    Fixed the date parsing in safari.

--- support.js
+++ support.js
@@ -39,7 +39,7 @@ _extendBasePrototype_(String,'trim',function() {
 });
 
 _extendBasePrototype_(String,'toDate',function() {
-  return new Date(this.toString());
+  return iso2date(this.toString());
 });
 
 /*
@@ -82,7 +82,7 @@ var time_in_words = function(timestamp) {
   var c = new Date();
   var t = null;
   if (typeof(timestamp) == "string") {
-    t = new Date(timestamp);
+    t = iso2date(timestamp);
   } else {
     t = timestamp;
   }
@@ -101,3 +101,32 @@ var time_in_words = function(timestamp) {
   if (dN > 0)   { return dN === 1? "1 minute ago" : dN + " minutes ago"; }
   return "less than a minute ago";
 };
+
+// from http://delete.me.uk/2005/03/iso8601.html
+var iso2date = function(string) {
+  var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+  "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\.([0-9]+))?)?" +
+  "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?";
+  var d = string.match(new RegExp(regexp));
+
+  var offset = 0;
+  var date = new Date(d[1], 0, 1);
+
+  if (d[3]) { date.setMonth(d[3] - 1); }
+  if (d[5]) { date.setDate(d[5]); }
+  if (d[7]) { date.setHours(d[7]); }
+  if (d[8]) { date.setMinutes(d[8]); }
+  if (d[10]) { date.setSeconds(d[10]); }
+  if (d[12]) { date.setMilliseconds(Number("0." + d[12]) * 1000); }
+  if (d[14]) {
+    offset = (Number(d[16]) * 60) + Number(d[17]);
+    offset *= ((d[15] === '-') ? 1 : -1);
+  }
+
+  offset -= date.getTimezoneOffset();
+  var time = (Number(date) + (offset * 60 * 1000));
+  var result = new Date();
+  result.setTime(Number(time));
+  return result;
+};
+
